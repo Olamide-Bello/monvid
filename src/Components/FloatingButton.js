bcaea59a02def57a44e4cf5573bbcdc102ac9c66
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-03-11 00:00:45 +0100

    added whatsapp button

--- src/Components/FloatingButton.js
+++ src/Components/FloatingButton.js
@@ -5,7 +5,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 function FloatingButton() {
     const message= `Hello, I want to enquire about your products and services`
     return (
-        <a href={`https://web.whatsapp.com/send?phone='+2349123532183'&text=${message}&app_absent=0`} target='_blank' rel="noreferrer" >
+        <a href={`https://wa.me/2349123532183?text=${message}&app_absent=0`} target='_blank' rel="noreferrer" >
             <button className='floating-btn'>
                 <FontAwesomeIcon icon={faWhatsapp} size="lg" /> Make Enquiry
             </button>
