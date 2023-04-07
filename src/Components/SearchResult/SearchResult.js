1307f1fd22d4dc4b2c92c60667e57a8ef64aa69b
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-04-07 23:32:09 +0100

    test

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { GlobalContext } from '../GlobalContext.js'
 import './SearchResult.css'
+import './Mobile.css'
+
 
 function SearchResult() {
     const { searchResult, searchMatch, searchValue, matches } = useContext(GlobalContext)
@@ -18,7 +20,7 @@ function SearchResult() {
                                 searchResult.map((item) => (
                                     <Link to={`/product/${item._id}`} key={item._id} className='result-link'>
                                         <div className='search-item' >
-                                            {/* <img src={item.image} alt="product" /> */}
+                                            <img src={item.image} alt="product" />
 
                                             <div>
                                                 <h5>{item.name}</h5>
