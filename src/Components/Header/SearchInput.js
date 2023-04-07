47e27de5160c467eba1a2563d27b45a923f6a463
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-04-07 23:07:01 +0100

    clean-up

--- src/Components/Header/SearchInput.js
+++ src/Components/Header/SearchInput.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../GlobalContext.js";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import SearchResult from "../SearchResult/SearchResult.js";
 
 function Search({ showSearch }) {
     const { searchParam, handleChange, matches, normalScreen } = useContext(GlobalContext)
@@ -10,6 +11,7 @@ function Search({ showSearch }) {
             <div id="search-area" className="searchArea">
                 <input id="search" name="search" value={searchParam} type="search" onChange={handleChange} className="input" placeholder="Enter product or category name" /><FontAwesomeIcon icon={faMagnifyingGlass} />
             </div>
+            <SearchResult/>
         </div>
     )
 }
