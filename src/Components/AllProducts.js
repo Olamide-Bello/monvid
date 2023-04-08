bbd2b826f5c7fff0be0744b54fc04d865ce20dee
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-04-08 01:25:07 +0100

    test

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -5,7 +5,7 @@ import { GlobalContext } from "./GlobalContext.js"
 
 function AllItems
   () {
-  const { allProducts, handleCart } = useContext(GlobalContext)
+  const { allProducts, handleCart, matches } = useContext(GlobalContext)
   const addToCart = (e) => {
     const qty = "1"
     handleCart(e.target.id, qty)
@@ -18,7 +18,7 @@ function AllItems
       <div className='all-products'>
         {allProducts.length > 1 ?
           allProducts.map((product) => (
-            <div key={product._id} className="product-card" style={{ background: `url(${product.image}) no-repeat center center/cover` }}>
+            <div key={product._id} className="product-card" style={!matches && { background: `url(${product.image}) no-repeat center center/cover` }}>
               <img src={product.image} alt="product" />
               <Container >
                 <div className="card-title">
@@ -26,7 +26,7 @@ function AllItems
                 </div>
                 <div>
                   <Link to={`/product/${product.category}/${product._id}`}><button className="card-btn">View product</button></Link>
-                  <button id={product._id} type='submit' className='card-add-cart-btn' onClick={addToCart}>Add to cart</button>
+                  {!matches && <button id={product._id} type='submit' className='card-add-cart-btn' onClick={addToCart}>Add to cart</button>}
                 </div>
               </Container>
             </div>
